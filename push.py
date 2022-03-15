a36bd7e61e3dd519665e7e5620640a93428a85f9
Oscar Solis <oscarsolis3301@gmail.com>
2022-03-15 00:34:59 -0700

    - Added message for when a user joins the server

--- push.py
+++ push.py
@@ -16,4 +16,11 @@ async def on_ready():
         f'{client.user} is online!\n'
     )
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+    
 client.run(bot_token)
