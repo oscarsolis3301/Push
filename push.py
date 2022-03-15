b9b28c7a773735cd6cbd607e03c5b018654a34df
Oscar Solis <oscarsolis3301@gmail.com>
2022-03-14 21:04:02 -0700

    Simple Discord bot adding a hello world command.

--- push.py
+++ push.py
@@ -1,9 +1,13 @@
 import discord
 
-class MyClient(discord.Client):
+client = discord.Client
+
+class MyClient(client):
     async def on_message(self, message):
         if message.author == self.user:
             return
 
         if message.content.startswith('$hello'):
             await message.channel.send('Hello World!')
+
+#client.run('1')
