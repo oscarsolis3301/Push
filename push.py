0d02bcd465ea3e2ad99a85904d96d0e546f7fa18
Oscar Solis <oscarsolis3301@gmail.com>
2022-03-16 14:55:09 -0700

    - Testing live share extension on VS code
    Co-authored-by: Angelina <AngelG4572@users.noreply.github.com>

--- push.py
+++ push.py
@@ -3,6 +3,7 @@
 
 import json
 import discord
+import asyncio
 
 with open('./config.json', 'r') as f:
     data = json.load(f)
@@ -12,6 +13,8 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    game=discord.Game("Minecraft")
+    await client.change_presence(status=discord.Status.idle, activity=game)
     print(
         f'{client.user} is online!\n'
     )
@@ -22,5 +25,23 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f'Hi {member.name}, welcome to my Discord server!'
     )
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$thumb'):
+        channel = message.channel
+        await channel.send('Send me that 👍 reaction, mate')
+
+        def check(reaction, user):
+            return user == message.author and str(reaction.emoji) == '👍'
+
+        try:
+            reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await channel.send('👎')
+        else:
+            await channel.send('👍')
     
 client.run(bot_token)
